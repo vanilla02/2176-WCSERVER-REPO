2aeef79224a493a702e95d1decb069f034b6acbf
vanilla02 <105054410+vanilla02@users.noreply.github.com>
2023-01-30 15:13:13 +0800

    Add files via upload

---
+++ addNumbers/add.js
@@ -0,0 +1,6 @@
+function add(){
+    let num1 = 8;
+    let num2 = 9;
+    module.exports = num1 + num2;
+}
+add();
---
+++ myGreetings/greet.js
@@ -0,0 +1,3 @@
+module.exports.greet = function(name1, name2){
+    return 'Hello ' + name1 + " & " + name2;
+}
