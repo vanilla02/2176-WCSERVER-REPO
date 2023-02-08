4f7cb851fd85828d616cc942b21b83006fdb67ea
vanilla02 <105054410+vanilla02@users.noreply.github.com>
2023-02-08 20:35:47 +0800

    Add files via upload

---
+++ Prelim-Hands-On-Exam/app.js
@@ -0,0 +1,7 @@
+const message = require('./message');
+
+const name = "Coleen";
+
+message.greet(name);
+message.congratulations(name);
+message.farewell(name);
---
+++ Prelim-Hands-On-Exam/message.js
@@ -0,0 +1,11 @@
+exports.greet = function(name) {
+    console.log('Hello,' + (name) + '!');
+  };
+  
+  exports.congratulations = function(name) {
+    console.log('Congratulations,' + (name) + '!');
+  };
+  
+  exports.farewell = function(name) {
+    console.log('Goodbye,' + (name) + '!');
+  };
