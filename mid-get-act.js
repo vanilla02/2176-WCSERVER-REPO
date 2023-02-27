e0c76c50e574a9d1157b8cc26954edcb49102226
vanilla02 <105054410+vanilla02@users.noreply.github.com>
2023-02-27 16:36:27 +0800

    Add files via upload

---
+++ mid-get-act.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const movies = [{
+    id: 1,
+    title: 'Superman'
+},
+{
+    id: 2,
+    title: 'Thor'
+},
+{
+    id: 3,
+    title: 'Iron Man'
+}
+];
+
+app.get('/api/movies', (req, res) => {
+    res.send(movies);
+});
+
+app.get('/api/movies/:id', (req, res) => {
+    const movie = movies.find(h => h.id === parseInt(req.params.id));
+    if (!movie) return res.status(404).send('The movie with the given ID was not found.');
+    res.send(movie);
+});
+
+app.listen(3000, () => console.log('Listening on port 3000'));
